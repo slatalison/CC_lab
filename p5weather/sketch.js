9364168267a60b122ca0759b54eaa4ab690a87cf
slatalison <53446374+slatalison@users.noreply.github.com>
2019-10-21 17:22:10 -0400

    P5 weather + web gazer

--- p5weather/sketch.js
+++ p5weather/sketch.js
@@ -10,8 +10,8 @@ var evenY=[];
 var drop=[];
 
 function setup() {
-  canvas=createCanvas(360, 640);
-  canvas.position(20,30);
+  canvas=createCanvas(window.innerWidth - 90, 640);
+  canvas.position(20,50);
   var button = select('#submit');
   button.mousePressed(weatherAsk);
   input = select('#city');
@@ -50,50 +50,54 @@ function draw() {
     var tomorrowhumidity = weather.list[1].humidity;
     noStroke();
     fill(92,179,204,todayhumidity);
-    ellipse(100, 230,todayhumidity, todayhumidity);
-    fill(92,179,204,tomorrowhumidity);
-    ellipse(250, 230,tomorrowhumidity,tomorrowhumidity);
+    ellipse(150, 130,todayhumidity * 10, todayhumidity * 10);
+    fill(251,103,104,tomorrowhumidity);
+    ellipse(150, 130,tomorrowhumidity * 10,tomorrowhumidity * 10);
     stroke(255);
-    line(0,evenY[0]+250,width,evenY[0]+250);
+    line(50,evenY[0]+190,width,evenY[0]+190);
     fill(255);
     noStroke();
     textSize(42);
-    text(weather.city.name,0, 80);
+    text(weather.city.name,50, 80);
     textSize(14);
-    text("Forecast: 16 Days/Daily ",0, 320);
-    fill(255);
-    text("Today:"+floor(todaytemp)+"\u2103",0, minY[0]+310);
-    text("Humidity: today VS tomorrow",0, 180);
+    text("TODAY  "+floor(todaytemp)+"\u2103",52, 240);
+    fill(255,80);
+    text("Forecast: 16 Days/Daily ",52, 260);
+    // text("humidity",0, 180);
+    // text("today vs tomorrow",0, 200);
+    
     
     for(var i=0;i<weather.list.length;i++){
+    let position = 190;
     fill(255,150);
       noStroke();
     textSize(12);
-    text(floor(weather.list[i].temp.eve), width*i/16,evenY[i]+260);   
-    stroke(255, 102, 0,30);
-      fill(255, 102, 0);
-      ellipse(width*i/16+5,evenY[i]+280,5,5);       
-    fill(255, 102, 0,10);
+    text(floor(weather.list[i].temp.eve), width*i/16 + 50,evenY[i]+215);   
+    stroke(244, 62, 6,30);
+      fill(239, 11, 72);
+      ellipse(width*i/16+50,evenY[i]+position,5,5);       
+    fill(239, 11, 72,5);
     beginShape();
-      vertex(0,evenY[0]+250);
-      curveVertex(5,evenY[0]+250);
-      curveVertex(width*1/16+5,evenY[1]+280);
-      curveVertex(width*2/16+5,evenY[2]+280);
-      curveVertex(width*3/16+5,evenY[3]+280);
-      curveVertex(width*4/16+5,evenY[4]+280);
-      curveVertex(width*5/16+5,evenY[5]+280);
-      curveVertex(width*6/16+5,evenY[6]+280);
-      curveVertex(width*7/16+5,evenY[7]+280);
-      curveVertex(width*8/16+5,evenY[8]+280);
-      curveVertex(width*9/16+5,evenY[9]+280);
-      curveVertex(width*10/16+5,evenY[10]+280);
-      curveVertex(width*11/16+5,evenY[11]+280);
-      curveVertex(width*12/16+5,evenY[12]+280);
-      curveVertex(width*13/16+5,evenY[13]+280);
-      curveVertex(width*14/16+5,evenY[14]+280);
-      curveVertex(width*15/16+5,evenY[15]+280);
-      curveVertex(width,evenY[0]+250);
-      vertex(width,evenY[0]+250);
+      let margin = 50;
+      vertex(50,evenY[0]+position);
+      curveVertex(50,evenY[0]+position);
+      curveVertex(width*1/16+margin,evenY[1]+position);
+      curveVertex(width*2/16+margin,evenY[2]+position);
+      curveVertex(width*3/16+margin,evenY[3]+position);
+      curveVertex(width*4/16+margin,evenY[4]+position);
+      curveVertex(width*5/16+margin,evenY[5]+position);
+      curveVertex(width*6/16+margin,evenY[6]+position);
+      curveVertex(width*7/16+margin,evenY[7]+position);
+      curveVertex(width*8/16+margin,evenY[8]+position);
+      curveVertex(width*9/16+margin,evenY[9]+position);
+      curveVertex(width*10/16+margin,evenY[10]+position);
+      curveVertex(width*11/16+margin,evenY[11]+position);
+      curveVertex(width*12/16+margin,evenY[12]+position);
+      curveVertex(width*13/16+margin,evenY[13]+position);
+      curveVertex(width*14/16+margin,evenY[14]+position);
+      curveVertex(width*15/16+margin,evenY[15]+position);
+      curveVertex(width,evenY[0]+position);
+      vertex(width,evenY[0]+position);
       endShape();
       
      // var checkrain=weather.list[0].weather.main;
@@ -104,10 +108,10 @@ function draw() {
          drop[j].show();
          drop[j].fall();         
         }//end for loop j
-         fill(255,15);
+         fill(255,30);
          noStroke();
          textSize(14);
-         text("It will rain in the next 16 days.", 0,100); 
+         text("It will rain in the next 16 days.", 52,120); 
          //text("There will be rain in the next"+i+"16 days.", 0,100); 
      }//end if checkrain
     }  
@@ -116,10 +120,16 @@ function draw() {
 
 function Drop() {
   this.x = random(0, width);
+  this.x1 = random(0, width);
+  this.x2 = random(0, width);
   this.y = random(-1000, 0);
+  this.y1 = random(-1000, 0);
+  this.y2 = random(-1000, 0);
   this.show = function() {
     stroke(255,15);
-    line(this.x, this.y, this.x, this.y+10);
+    line(this.x, this.y, this.x, this.y + random(15,22));
+    line(this.x1, this.y1, this.x1, this.y1 + random(12,18));
+    line(this.x2, this.y2, this.x2, this.y2 + random(12,18));
      if(this.y>height){
       this.y=0;
     }
